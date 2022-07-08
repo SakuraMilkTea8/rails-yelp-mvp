02feace3cc362551ed50015ec8334a3ac75b48ae
Ellie Cornish <lizthatsme@gmail.com>
2022-07-09 06:36:26 +0900

    Structure complete. Missing CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'seeding..'
+
+5.times do |f|
+  resto = Restaurant.new(name: Faker::Hipster.word, address: Faker::Address.street_address, category: %w[chinese italian japanese french belgian][rand(0..4)])
+  resto.save
+end
+
+puts 'Done!'
